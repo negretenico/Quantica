e2410ef4c97ffa5e24c6c4ce5b6b0846ae534c79
negretenico <negretenico0@gmail.com>
2025-09-02 12:02:17 -0400

    adding small changes

---
+++ markettransformer/src/main/java/com/negretenico/quantica/markettransformer/MarketTransformerApplication.java
@@ -0,0 +1,13 @@
+package com.negretenico.quantica.markettransformer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketTransformerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketTransformerApplication.class, args);
+	}
+
+}
---
+++ markettransformer/src/test/java/com/negretenico/quantica/markettransformer/MarketTransformerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.negretenico.quantica.markettransformer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketTransformerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

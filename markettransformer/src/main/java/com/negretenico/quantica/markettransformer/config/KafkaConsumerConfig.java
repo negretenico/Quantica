35236d3acaa2e4223ca061731e253fc0148f766e
Nicholas A Negrete <57291001+negretenico@users.noreply.github.com>
2026-08-16 11:38:06 -0400

    Revert "Fix/markettrade markettransformer crash loop"

--- markettransformer/src/main/java/com/negretenico/quantica/markettransformer/config/KafkaConsumerConfig.java
+++ markettransformer/src/main/java/com/negretenico/quantica/markettransformer/config/KafkaConsumerConfig.java
@@ -10,7 +10,6 @@ import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
@@ -24,7 +23,6 @@ import org.springframework.util.backoff.FixedBackOff;
 import java.util.Map;
 
 @Slf4j
-@EnableKafka
 @Configuration
 public class KafkaConsumerConfig {
 	private final KafkaProperties kafkaProperties;
--- markettransformer/src/main/java/com/negretenico/quantica/markettransformer/model/BinanceStreamResponse.java
+++ markettransformer/src/main/java/com/negretenico/quantica/markettransformer/model/BinanceStreamResponse.java
@@ -1,7 +1,6 @@
 package com.negretenico.quantica.markettransformer.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.math.BigDecimal;
@@ -20,7 +19,6 @@ import java.math.BigDecimal;
  * "m": true               // Is buyer market maker (true=sell, false=buy)
  * }
  */
-@JsonIgnoreProperties(ignoreUnknown = true)
 public record BinanceStreamResponse(
 		@JsonProperty("e") String eventType,
 		@JsonProperty("E") long eventTime,
